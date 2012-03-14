9c1815feddb9ece3852f7d30486de93742e3d623
Kyle Meyer <kaiuhl@gmail.com>
2012-03-14 13:24:59 -0700

    formatting fix

--- lib/pretty_param.rb
+++ lib/pretty_param.rb
@@ -4,9 +4,9 @@ module PrettyParam
 		def escape(string)
 			result = string
 			result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
-      result.gsub!(/[^\w_ \-]+/i,   '') # Remove unwanted chars.
-      result.gsub!(/[ \-]+/i,      '-') # No more than one of the separator in a row.
-      result.gsub!(/^\-|\-$/i,      '') # Remove leading/trailing separator.
+			result.gsub!(/[^\w_ \-]+/i,		'') # Remove unwanted chars.
+			result.gsub!(/[ \-]+/i,			 '-') # No more than one of the separator in a row.
+			result.gsub!(/^\-|\-$/i,			'') # Remove leading/trailing separator.
 			result.downcase
 		end
 	end
@@ -15,8 +15,8 @@ module PrettyParam
 		# Takes 1 or more symbols and stores them in class.pretty_params,
 		# and then overrides to_param
 		def has_pretty_param(*fields)			
-			define_singleton_method	:pretty_params, -> { fields }
-			define_method	:to_param do
+			define_singleton_method :pretty_params, -> { fields }
+			define_method :to_param do
 				PrettyParam.escape("#{self.id}-#{self.class.pretty_params.map{|a| self.send(a.to_sym) }.join('-')}")
 			end
 		end
