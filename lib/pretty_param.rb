d32edca81b4c37a3028fac3f696043ff20bc1ad7
Kyle Meyer <kaiuhl@gmail.com>
2012-03-14 13:27:32 -0700

    formatting fix

--- lib/pretty_param.rb
+++ lib/pretty_param.rb
@@ -4,9 +4,9 @@ module PrettyParam
 		def escape(string)
 			result = string
 			result.gsub!(/[^\x00-\x7F]+/, '') # Remove anything non-ASCII entirely (e.g. diacritics).
-			result.gsub!(/[^\w_ \-]+/i,		'') # Remove unwanted chars.
-			result.gsub!(/[ \-]+/i,			 '-') # No more than one of the separator in a row.
-			result.gsub!(/^\-|\-$/i,			'') # Remove leading/trailing separator.
+			result.gsub!(/[^\w_ \-]+/i, '') # Remove unwanted chars.
+			result.gsub!(/[ \-]+/i, '-') # No more than one of the separator in a row.
+			result.gsub!(/^\-|\-$/i, '') # Remove leading/trailing separator.
 			result.downcase
 		end
 	end
