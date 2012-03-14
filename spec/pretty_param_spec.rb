4c42e7d74393b64f3be803d40d97a7b507ccc1cf
Kyle Meyer <kaiuhl@gmail.com>
2012-03-14 13:26:35 -0700

    Remove half-done testing suite until ready

--- spec/pretty_param_spec.rb
+++ spec/pretty_param_spec.rb
@@ -1,22 +1,5 @@
-require 'active_record'
-require_relative '../lib/pretty_param'
-
-class TestObject < ActiveRecord::Base
-	def title
-		"Exciting, yet complicated (!@@&*) title  "
-	end
-	def name
-		"and a name!"
-	end
-	def id
-		14
-	end
-end
-
 describe PrettyParam do
-	let!(:object) { TestObject.new }
-	
-	it "shouldn't interfere with ActiveRecord without being called" do
-		object.to_param.should eq(14)
+	it "should have tests but doesn't yet because testing Rails outside of Rails is a pain. Also, the source code to this gem is like 15 lines." do
+		1.should eq(2)
 	end
 end
