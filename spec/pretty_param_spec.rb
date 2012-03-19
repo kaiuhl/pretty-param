e790ae800822f0eb119d500c8fc94cacc0134bb6
Kyle Meyer <kaiuhl@gmail.com>
2012-03-19 16:09:24 -0700

    Basic functional tests

--- spec/pretty_param_spec.rb
+++ spec/pretty_param_spec.rb
@@ -1,5 +1,32 @@
+require 'rails/all'
+require 'pretty_param'
+
+ActiveRecord::Base.establish_connection(adapter: "sqlite3",	database: "test.db")
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS 'dummies'")
+ActiveRecord::Base.connection.create_table(:dummies) do |t|
+	t.string :first_name
+	t.string :last_name
+	t.string :nickname
+end
+
+class Dummy < ActiveRecord::Base
+	has_pretty_param :first_name, :last_name
+end
+
 describe PrettyParam do
-	it "should have tests but doesn't yet because testing Rails outside of Rails is a pain. Also, the source code to this gem is like 15 lines." do
-		1.should eq(2)
+	let(:dummy) { Dummy.create(first_name: "Alex", last_name: "Kiheri", nickname: "AFK") }
+	let(:blinged_dummy) { Dummy.create(first_name: "^A%lex@#", last_name: "#K+iheri!@") }
+	let(:multiple_space_dummy) { Dummy.create(first_name: "Alex ", last_name: " Kiheri") }
+	
+	it "should add specified attributes to `to_param`" do
+		dummy.to_param.should include("alex-kiheri")
+	end
+	
+	it "should filter out symbols" do
+		blinged_dummy.to_param.should include("alex-kiheri")
+	end
+	
+	it "should remove multiple hyphens" do
+			multiple_space_dummy.to_param.should include("alex-kiheri")
 	end
 end
